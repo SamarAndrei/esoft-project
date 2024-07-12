52645001a4b8c9b50197f192e011afd1e99396b0
Andrei Samar <istore103@icloud.com>
2024-07-12 17:10:34 +0500

    fix

--- BackendApp/Index.js
+++ BackendApp/Index.js
@@ -17,6 +17,26 @@ app.use(
     }),
 );
 app.use(cookieParser());
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname1, '../reactProjectApp/dist')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(
+                __dirname1,
+                '../reactProjectApp',
+                'dist',
+                'index.html',
+            ),
+        ),
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running..');
+    });
+}
 
 const RolesModel = require('./reposio/rolesDal');
 const UserModel = require('./reposio/userDal');
