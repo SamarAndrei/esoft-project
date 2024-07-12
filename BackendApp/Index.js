b20e2ce886e0563f9a8fa9de634a467f583e16e2
Andrei Samar <istore103@icloud.com>
2024-07-12 18:26:48 +0500

    fix

--- BackendApp/Index.js
+++ BackendApp/Index.js
@@ -19,27 +19,6 @@ app.use(
 );
 app.use(cookieParser());
 
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname1, '/reactProjectApp/dist')));
-
-    app.get('*', (req, res) =>
-        res.sendFile(
-            path.resolve(
-                __dirname1,
-                'reactProjectApp',
-                'dist',
-                'index.html',
-            ),
-        ),
-    );
-} else {
-    app.get('/', (req, res) => {
-        res.send('API is running..');
-    });
-}
-
 const RolesModel = require('./reposio/rolesDal');
 const UserModel = require('./reposio/userDal');
 const userRoutes = require('./routes/userRoutes');
