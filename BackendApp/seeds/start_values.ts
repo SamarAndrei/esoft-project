e584277d50928f0b82c495aa5d87f878a87e026b
Andrei Samar <istore103@icloud.com>
2024-07-12 15:03:48 +0500

    fix

--- BackendApp/seeds/start_values.ts
+++ BackendApp/seeds/start_values.ts
@@ -2,7 +2,7 @@ import { Knex } from "knex";
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
-    await knex("table_name").del();
+    // await knex("table_name").del();
 
     // Inserts seed entries
     await knex("roles").insert([
